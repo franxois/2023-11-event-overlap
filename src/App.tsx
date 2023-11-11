6b7301f08027a519b22c61b1fe2cbef422e3d513
Franxois <franxoois@gmail.com>
2023-11-11 13:34:32 +0100

    fix(event): fix event 5 width

--- src/App.tsx
+++ src/App.tsx
@@ -27,51 +27,59 @@ const getPercentFullHeight = (minutes: number) => {
 export const App: FC = () => {
   const events = Object.fromEntries(
     input
-      .map(({ id, start, duration }) => {
-        const overlap = input
-          .filter(({ id: oId, start: oStart, duration: oDuration }) => {
-            if (id === oId) return false;
-
-            const timeStart = timeToMinutes(start);
-            const oTimeStart = timeToMinutes(oStart);
-            const oEndTime = oTimeStart + oDuration;
-
-            if (oEndTime <= timeStart) {
-              // Other event finish before
-              return false;
-            }
-
-            if (oTimeStart >= timeStart + duration) {
-              // Other event start after
-              return false;
-            }
-
-            return true;
-          })
-          .map((event) => event.id);
-
-        return {
+      .map(
+        ({
           id,
           start,
           duration,
-          overlap,
-          hOffset: undefined,
-          width: undefined,
-        } as {
+        }): {
           id: number;
           start: StartTime;
           duration: number;
           overlap: number[];
-          hOffset: number | undefined;
-          width: number | undefined;
-        };
-      })
-      .sort((a, b) => (a.overlap.length < b.overlap.length ? -1 : 1))
+          hOffset?: number;
+          width?: number;
+        } => {
+          const overlap = input
+            .filter(({ id: oId, start: oStart, duration: oDuration }) => {
+              if (id === oId) return false;
+
+              const timeStart = timeToMinutes(start);
+              const oTimeStart = timeToMinutes(oStart);
+              const oEndTime = oTimeStart + oDuration;
+
+              if (oEndTime <= timeStart) {
+                // Other event finish before
+                return false;
+              }
+
+              if (oTimeStart >= timeStart + duration) {
+                // Other event start after
+                return false;
+              }
+
+              return true;
+            })
+            .map((event) => event.id);
+
+          return {
+            id,
+            start,
+            duration,
+            overlap,
+          };
+        }
+      )
       .map((event) => [event.id, event])
   );
 
-  for (const id in events) {
+  const idsSortedByOverlap = Object.keys(events).sort((a, b) =>
+    events[a].overlap.length < events[b].overlap.length ? 1 : -1
+  );
+  for (const id of idsSortedByOverlap) {
     const event = events[id];
+
+    console.log("process", id, event);
 
     if (event.overlap.length === 0) continue;
 
@@ -88,12 +96,18 @@ export const App: FC = () => {
 
     // TODO : if there is already a width set, use it instead of 100%/count
 
-    const widthPercent = 100 / (myOverlapLen + 1);
+    let widthPercent = 100 / (myOverlapLen + 1);
 
     const ids = [id, ...event.overlap].sort((a, b) =>
       // Sort by event duration descending
       events[a].duration > events[b].duration ? -1 : 1
     );
+
+    const minOfCurrentWidths = Math.min(
+      ...ids.map((i) => events[i].width || 100)
+    );
+
+    if (widthPercent > minOfCurrentWidths) widthPercent = minOfCurrentWidths;
 
     for (let offset = 0; offset < ids.length; offset++) {
       const existingEventWithThisOffset = ids
